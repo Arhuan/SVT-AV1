102e177f1f8612e582f15d7eab07f87c4cee14e9
hguermaz <hassen.guermazi@intel.com>
2020-09-28 11:21:32 -0700

    20_35

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -2510,7 +2510,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
     else if (pd_pass == PD_PASS_1)
         context_ptr->md_exit_th = 0;
     else
-        context_ptr->md_exit_th = 30;
+        context_ptr->md_exit_th = 35;
 #endif
     if (pd_pass == PD_PASS_0)
         context_ptr->interpolation_search_level = IFS_OFF;
